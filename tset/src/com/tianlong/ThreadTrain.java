9ae13f12f8d4bf91be7d9a3280c8957749008067
jiyi <jiyi327li@163.com>
2019-09-22 18:53:31 +0800

    提交

---
+++ tset/src/com/tianlong/ThreadTrain.java
@@ -0,0 +1,15 @@
+package com.tianlong;
+
+/**
+ * @Author : tianlong
+ * @ClassName :ThreadTrain
+ * @Date 2019/9/2
+ * @Description TODO
+ */
+public class ThreadTrain implements Runnable {
+    @Override
+    public void run() {
+
+    }
+
+}
